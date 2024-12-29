dd3d308fcf5047c5bcc9baa18ff88481c52868b9
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2024-12-29 19:00:07 +0800

    Update action_util.sh

--- action_util.sh
+++ action_util.sh
@@ -6,6 +6,7 @@ function version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" 
 function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1"; }
 function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
 
+
 function update_info()
 {
     cd $GITHUB_WORKSPACE
@@ -25,14 +26,5 @@ function update_info()
         git config user.email github-actions@github.com
         git commit -a -m "$APP_NAME-$APP_LATEST_TAG release"
         git push
-    fi
-}
-
-function webdav_upload()
-{
-    if [[ "$WEBDAV_UPLOAD" == "true" ]] && [[ -n "$WEBDAV_AUTH" ]] && [[ -n "$WEBDAV_URL" ]] && [[ "$REPO_OWNER" == "$REPO_ACTOR" ]]; then
-        debug "已开启webdav上传"
-        curl -X MKCOL -u $WEBDAV_AUTH $WEBDAV_URL
-        curl -u $WEBDAV_AUTH -T $1 $WEBDAV_URL
     fi
 }
